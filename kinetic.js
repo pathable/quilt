ac2b2fe0e2c4e75847874587ffc22a9f72cbcf00
Brad Dunbar <dunbarb2@gmail.com>
2012-04-25 11:44:31 -0400

    Tweak destroy just a bit.

--- kinetic.js
+++ kinetic.js
@@ -63,9 +63,14 @@
     // Destroy child views and ensure that references to this view are
     // eliminated to prevent memory leaks.
     destroy: function() {
-      for (var i = 0; i < this.views.length; i++) {
-        if (this.views[i].destroy) this.views[i].destroy();
+      var view;
+
+      // Destroy child views.
+      while (view = this.views.pop()) {
+        if (view.destroy) view.destroy();
       }
+
+      // Clean up event handlers.
       if (this.model) this.model.off(null, null, this);
       if (this.collection) this.collection.off(null, null, this);
     },
